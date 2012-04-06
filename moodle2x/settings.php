48396eb2c4bff2250b3edabbd3846e8a2295b962
azeckoski@gmail.com <azeckoski@gmail.com@14a20f9c-d605-11de-986c-23549013a0f8>
2012-04-06 01:36:58 +0000

    Adding in more of the initial files

--- moodle2x/settings.php
+++ moodle2x/settings.php
@@ -1,5 +1,23 @@
 <?php
-/* $Id: settings.php 1169 2011-12-23 17:17:31Z aaronz $ */
+/**
+ * Copyright (c) 2012 i>clicker (R) <http://www.iclicker.com/dnn/>
+ *
+ * This file is part of i>clicker Moodle integrate.
+ *
+ * i>clicker Moodle integrate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * i>clicker Moodle integrate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with i>clicker Moodle integrate.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ /* $Id: settings.php 1169 2011-12-23 17:17:31Z aaronz $ */
 
 // control the config settings for this plugin
 require_once ('bfwpub_service.php');
--- moodle2x/version.php
+++ moodle2x/version.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Copyright (c) 2012 i>clicker (R) <http://www.iclicker.com/dnn/>
+ *
+ * This file is part of i>clicker Moodle integrate.
+ *
+ * i>clicker Moodle integrate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * i>clicker Moodle integrate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with i>clicker Moodle integrate.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /* $Id: version.php 1169 2011-12-23 17:17:31Z aaronz $ */
 
 $plugin->version = 2012040500;
